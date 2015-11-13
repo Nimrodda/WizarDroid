662dfc8fa3a5273af7247c865d242d3ae6b13eeb
Juraj Suchán <juraj.suchan@gmail.com>
2015-11-13 14:51:48 +0100

    CustomViewPager

---
+++ wizardroid/src/main/java/org/codepond/wizardroid/layouts/CustomViewPager.java
@@ -0,0 +1,48 @@
+package org.codepond.wizardroid.layouts;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by virtualvoid on 13.11.2015.
+ */
+public class CustomViewPager extends ViewPager {
+
+    private boolean swipeEnabled;
+
+    public CustomViewPager(Context context) {
+        super(context);
+    }
+
+    public CustomViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (this.swipeEnabled) {
+            return super.onTouchEvent(event);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        if (this.swipeEnabled) {
+            return super.onInterceptTouchEvent(event);
+        }
+
+        return false;
+    }
+
+    public boolean isSwipeEnabled() {
+        return swipeEnabled;
+    }
+
+    public void setSwipeEnabled(boolean swipeEnabled) {
+        this.swipeEnabled = swipeEnabled;
+    }
+}
